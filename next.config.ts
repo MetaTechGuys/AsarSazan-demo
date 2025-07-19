209db38f328338f6d4a9bd5dfaf80a4fb69fde9d
Ali Ghorbani <alidoubleplus@gmail.com>
2025-07-19 20:04:55 +0330

    re install npm

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,17 @@
 import type { NextConfig } from 'next'
-import createMDX from '@next/mdx'
-import remarkFrontmatter from 'remark-frontmatter'
+// import createMDX from '@next/mdx'
+// import remarkFrontmatter from 'remark-frontmatter'
 
 const nextConfig: NextConfig = {
-  experimental: {
-    viewTransition: true,
-  },
   pageExtensions: ['md', 'mdx', 'ts', 'tsx'],
 }
 
-const withMDX = createMDX({
-  extension: /\.(md|mdx)$/,
-  options: {
-    remarkPlugins: [remarkFrontmatter],
-  },
-})
+// const withMDX = createMDX({
+//   extension: /\.(md|mdx)$/,
+//   options: {
+//     remarkPlugins: [remarkFrontmatter],
+//   },
+// })
 
 // Merge MDX config with Next.js config
-export default withMDX(nextConfig)
+export default nextConfig // withMDX(nextConfig)
