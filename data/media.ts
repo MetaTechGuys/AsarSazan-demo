f602c16ba0492beeb811cb5d589f961660019926
Ali Ghorbani <alidoubleplus@gmail.com>
2025-07-19 18:25:37 +0330

    replace images

--- data/media.ts
+++ data/media.ts
@@ -1,29 +1,29 @@
 import { StaticImageData } from 'next/image'
-import slide1 from '@/assets/slides/slide-0.jpg'
-import slide2 from '@/assets/slides/slide-1.jpg'
-import slide3 from '@/assets/slides/slide-2.jpg'
+import slide1 from '@/assets/slides/slide-0.webp'
+import slide2 from '@/assets/slides/slide-1.webp'
+import slide3 from '@/assets/slides/slide-2.webp'
 
-import story1 from '@/assets/stories/story-1.jpg'
-import story2 from '@/assets/stories/story-2.jpg'
-import story3 from '@/assets/stories/story-3.jpg'
-import story4 from '@/assets/stories/story-4.jpg'
+import story1 from '@/assets/stories/story-1.webp'
+import story2 from '@/assets/stories/story-2.webp'
+import story3 from '@/assets/stories/story-3.webp'
+import story4 from '@/assets/stories/story-4.webp'
 
-import project1 from '@/assets/projects/projects-01-02.jpg'
-import project2 from '@/assets/projects/projects-01-03.jpg'
-import project3 from '@/assets/projects/projects-01-04.jpg'
-import project4 from '@/assets/projects/projects-01-05.jpg'
-import project5 from '@/assets/projects/projects-01-06.jpg'
-import project6 from '@/assets/projects/projects-01-07.jpg'
+import project1 from '@/assets/projects/projects-01-02.webp'
+import project2 from '@/assets/projects/projects-01-03.webp'
+import project3 from '@/assets/projects/projects-01-04.webp'
+import project4 from '@/assets/projects/projects-01-05.webp'
+import project5 from '@/assets/projects/projects-01-06.webp'
+import project6 from '@/assets/projects/projects-01-07.webp'
 
 import projectBg from '@/assets/projects/project-bg.png'
 import projectItem from '@/assets/projects/project-item.png'
 
-import marashi from '@/assets/testimonials/marashi.jpg'
+import marashi from '@/assets/testimonials/marashi.webp'
 import mohebi from '@/assets/testimonials/mohebi.webp'
 import rajabi from '@/assets/testimonials/rajabi.webp'
-import zahedi from '@/assets/testimonials/zahedi.jpg'
+import zahedi from '@/assets/testimonials/zahedi.webp'
 
-import lastHero from '@/assets/last-hero-bg.jpg'
+import lastHero from '@/assets/last-hero-bg.webp'
 
 type MediaMap = Record<string, StaticImageData>
 
